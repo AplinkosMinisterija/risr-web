8d414d9d3c93f45510ef67a531ce122d2544d864
LWangllix <vanglikas.lukas@gmail.com>
2023-12-05 15:17:31 +0200

    return button

--- src/pages/CantLogin.tsx
+++ src/pages/CantLogin.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import ReturnToLogin from "../components/other/ReturnToLogin";
 import { device } from "../styles";
 import { descriptions, formLabels } from "../utils/texts";
 
@@ -7,31 +8,27 @@ export const CantLogin = () => {
     <>
       <H1>{formLabels.inActiveProfile}</H1>
       <Description>{descriptions.cantLogin} </Description>
+      <ReturnToLogin />
     </>
   );
 };
 
 const Description = styled.div`
-  text-align: center;
   font-size: 1.6rem;
-  line-height: 26px;
   letter-spacing: 0px;
   color: #7a7e9f;
-  width: 70%;
+  margin-bottom: 40px;
   @media ${device.mobileL} {
     width: 100%;
   }
 `;
 
 const H1 = styled.h1`
-  text-align: center;
   font-weight: bold;
   font-size: 3.2rem;
-  line-height: 44px;
+  line-height: 22px;
   letter-spacing: 0px;
-  color: #121a55;
-  opacity: 1;
-  margin: 0px 0px 16px 0px;
+  margin-top: 40px;
 
   @media ${device.mobileL} {
     padding-bottom: 0px;
