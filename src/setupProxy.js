c8b326776c9a62d50a0bcc5a8a476f34f04bc8af
Dovile <dovile.maminskaite@am.lt>
2024-03-18 17:31:24 +0100

    sukeisti adresai

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "https://dev.risr.biip.lt/api",
+      target: "https://dev-risr.biip.lt/api",
       changeOrigin: true,
       pathRewrite: {
         "^/api": ""
